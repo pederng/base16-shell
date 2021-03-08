9fcbfb7fc2f86593bb502aa4134e4ad757481a32
Peder Notto Galteland <peder.galteland@softwarelab.no>
2021-03-08 13:37:14 +0100

    New themes

--- scripts/base16-vice.sh
+++ scripts/base16-vice.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 # base16-shell (https://github.com/chriskempson/base16-shell)
 # Base16 Shell template by Chris Kempson (http://chriskempson.com)
-# PaperColor Light scheme by Jon Leopard (http://github.com/jonleopard) based on PaperColor Theme (https://github.com/NLKNguyen/papercolor-theme)
+# vice scheme by Thomas Leon Highbaugh thighbaugh@zoho.com
 
-color00="ee/ee/ee" # Base 00 - Black
-color01="bc/bc/bc" # Base 08 - Red
-color02="87/00/af" # Base 0B - Green
-color03="d7/00/87" # Base 0A - Yellow
-color04="d7/5f/00" # Base 0D - Blue
-color05="00/5f/af" # Base 0E - Magenta
-color06="d7/5f/00" # Base 0C - Cyan
-color07="44/44/44" # Base 05 - White
-color08="5f/87/00" # Base 03 - Bright Black
+color00="1F/20/23" # Base 00 - Black
+color01="ff/9c/ff" # Base 08 - Red
+color02="6f/ff/ff" # Base 0B - Green
+color03="f0/ee/6e" # Base 0A - Yellow
+color04="00/ca/ff" # Base 0D - Blue
+color05="50/B0/FF" # Base 0E - Magenta
+color06="00/ea/ff" # Base 0C - Cyan
+color07="8b/9c/be" # Base 05 - White
+color08="3A/3F/49" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
 color10=$color02 # Base 0B - Bright Green
 color11=$color03 # Base 0A - Bright Yellow
 color12=$color04 # Base 0D - Bright Blue
 color13=$color05 # Base 0E - Bright Magenta
 color14=$color06 # Base 0C - Bright Cyan
-color15="87/87/87" # Base 07 - Bright White
-color16="d7/00/00" # Base 09
-color17="00/5f/87" # Base 0F
-color18="af/00/00" # Base 01
-color19="00/87/00" # Base 02
-color20="00/87/af" # Base 04
-color21="00/5f/87" # Base 06
-color_foreground="44/44/44" # Base 05
-color_background="ee/ee/ee" # Base 00
+color15="e9/ef/ff" # Base 07 - Bright White
+color16="79/6a/f5" # Base 09
+color17="f8/50/ac" # Base 0F
+color18="23/26/2C" # Base 01
+color19="31/30/40" # Base 02
+color20="52/5d/73" # Base 04
+color21="B2/BF/D9" # Base 06
+color_foreground="8b/9c/be" # Base 05
+color_background="1F/20/23" # Base 00
 
 if [ -n "$TMUX" ]; then
   # Tell tmux to pass the escape sequences through
@@ -78,13 +78,13 @@ put_template 21 $color21
 # foreground / background / cursor color
 if [ -n "$ITERM_SESSION_ID" ]; then
   # iTerm2 proprietary escape codes
-  put_template_custom Pg 444444 # foreground
-  put_template_custom Ph eeeeee # background
-  put_template_custom Pi 444444 # bold color
-  put_template_custom Pj 008700 # selection color
-  put_template_custom Pk 444444 # selected text color
-  put_template_custom Pl 444444 # cursor
-  put_template_custom Pm eeeeee # cursor text
+  put_template_custom Pg 8b9cbe # foreground
+  put_template_custom Ph 1F2023 # background
+  put_template_custom Pi 8b9cbe # bold color
+  put_template_custom Pj 313040 # selection color
+  put_template_custom Pk 8b9cbe # selected text color
+  put_template_custom Pl 8b9cbe # cursor
+  put_template_custom Pm 1F2023 # cursor text
 else
   put_template_var 10 $color_foreground
   if [ "$BASE16_SHELL_SET_BACKGROUND" != false ]; then
